d8a39067bd118ad34610a52ed306ef996cf7d221
Hajar Mertah <hajar.mertah12@gmail.com>
2023-08-13 11:58:30 +0100

    commit 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen.js';
+import ProductScreen from './screens/ProductScreen.js';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">MARKETPLACE</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">MARKETSPACE</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
