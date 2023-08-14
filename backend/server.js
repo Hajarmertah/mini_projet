2efbefa5e76db71f32674683356d125fee34ee8a
Hajar Mertah <hajar.mertah12@gmail.com>
2023-08-14 18:02:46 +0100

    commit 7

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
-app.use(cors());
+app.use('/api/seed', seedRouter);
 
+app.use(cors());
+//test
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
