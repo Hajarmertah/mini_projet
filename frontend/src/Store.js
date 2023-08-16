d7a22902055c9a6e5081cca914dafdb0c02546aa
Hajar Mertah <hajar.mertah12@gmail.com>
2023-08-16 01:28:35 +0100

    commit 11

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -9,7 +9,7 @@ const initialState = {
 
   cart: {
     shippingAddress: localStorage.getItem('shippingAddress')
-      ? JSON.parse(localStorage.getItem('shippinAddress'))
+      ? JSON.parse(localStorage.getItem('shippingAddress'))
       : {},
     paymentMethod: localStorage.getItem('paymentMethod')
       ? localStorage.getItem('paymentMethod')
@@ -41,6 +41,9 @@ function reducer(state, action) {
       localStorage.setItem('cartItems', JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
+    case 'CART_CLEAR':
+      return { ...state, cart: { ...state.cart, cartItems: [] } };
+
     case 'USER_SIGNIN':
       return { ...state, userInfo: action.payload };
     case 'USER_SIGNOUT':
